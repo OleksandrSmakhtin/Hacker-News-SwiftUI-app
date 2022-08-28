cdeb9e6ed6ed335e03871d01959c1db239d606d8
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2022-08-28 22:04:23 +0300

    Initial Commit

---
+++ Hacker News SwiftUI app/Hacker_News_SwiftUI_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News_SwiftUI_appApp.swift
+//  Hacker News SwiftUI app
+//
+//  Created by Oleksandr Smakhtin on 28.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News_SwiftUI_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
